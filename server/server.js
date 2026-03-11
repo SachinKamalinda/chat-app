a0bffcc6329f43e8784802fbd93400659fc08cff
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-11 13:10:06 +0530

    Configure the backend libraris and connect to mongo DB

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from 'http';
+import { connectDB } from './lib/db.js';
+
+//Create Express app and HTTP server
+const app = express();
+const server = http.createServer(app);
+
+//Middleware
+app.use(express.json({limit: '4mb'}));
+app.use(cors());
+
+//Routes
+app.use("/api/status", (req, res)=> res.send("Server is running"));
+
+// Connect to MongoDB
+await connectDB();
+
+//Start server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT:" 
++ PORT));
