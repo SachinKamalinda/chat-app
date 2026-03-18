2f15a1e0f42085e16e9f2eafb106618dcbcae116
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-18 15:30:32 +0530

    implement real-time messaging with Socket.io and cloudinary image upload

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
-
+import cloudinary from "../lib/cloudinary.js";
+import {io, userSocketMap} from "../server.js";
 
 // Get all users expect the logged in user
 export const getUsersForSidebar = async (req, res) => {
@@ -59,3 +60,35 @@ export const markMessagesAsSeen = async (req, res) => {
 }
 
 // Send messages to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let  imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        // Emit the new message to the receiver if they are online
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
+        res.json({success: true, newMessage})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,10 +5,38 @@ import http from 'http';
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
 
 //Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+// Create Socket.io server
+export const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+})
+
+// Store online users
+export const userSocketMap = {}; // {userId: socketId}
+
+// Handle Socket.io connections
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected: ", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: ", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
 
 //Middleware
 app.use(express.json({limit: '4mb'}));
