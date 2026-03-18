0a5e547b14e042a610caf97d8722cfb34d4376f5
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-18 15:37:13 +0530

    fix: update import statements to include file extensions for consistency

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { login, signup, updateProfile } from "../controllers/userController";
-import { protectRoute } from "../middleware/auth";
+import { login, signup, updateProfile } from "../controllers/userController.js";
+import { protectRoute } from "../middleware/auth.js";
+import { checkAuth } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
