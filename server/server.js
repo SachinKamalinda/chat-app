457413224221f87b25f2629014d9d93f0c26e048
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-19 18:41:21 +0530

    fix: update background image path in App component and add SVG file

--- server/server.js
+++ server/server.js
@@ -50,7 +50,12 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-//Start server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT:" 
-+ PORT));
+
+if(process.env.NODE_ENV !== "production"){
+    //Start server
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT:" 
+    + PORT));
+}
+// Export server for versel
+export default server;
