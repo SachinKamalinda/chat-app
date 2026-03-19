dc09719effd1f3b3961a0ad3bb5ec294865ef728
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-19 13:15:17 +0530

    fix: remove unused mongoose import from ChatContext

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { set } from "mongoose";
 import toast from "react-hot-toast";
 
 
