0c62b8aeef94a0cc94e2afd35814e00a8e69d0b6
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-19 10:05:30 +0530

    feat: add AuthContext for user authentication and socket connection management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster} from 'react-hot-toast'
 import { useContext } from 'react'
-import { AuthContext } from '../constext/AuthContext'
+import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
   const { authUser } = useContext (AuthContext)
