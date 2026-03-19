9272b2221ccac475c3dbaa24890b5d7c83384506
Sachiya-coder <sachinkamalinda@gmail.com>
2026-03-19 18:08:04 +0530

    feat: update Sidebar, RightSidebar, and HomePage for improved user selection and message handling

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
 
-    const [selectedUser, setSelectdUser] = useState(false)
+    const {selectedUser} = useContext(ChatContext)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
@@ -15,7 +16,7 @@ const HomePage = () => {
       `}>
         <Sidebar  />
         <ChatContainer  />
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectdUser} />
+        <RightSidebar  />
       </div>
       
     </div>
